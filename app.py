ef3882ff9395a272869b5b6c4ab4e5c6f291716c
JonnyDdesign <jondesserres@gmail.com>
2024-04-02 11:55:39 -0700

    added an add function to allow user to add a todo item

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -13,12 +13,18 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
+    # Show todo list
     todo_list = Todo.query.all()
     return render_template('index.html', todo_list=todo_list)
 
-@app.route('/about')
-def about():
-    return "About"
+@app.route('/add', methods=["GET", "POST"])
+def add():
+    # Add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     with app.app_context():
